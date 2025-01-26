8b2e0ccdd295783f6e827ea26ed38b218543accb
almuLR <a.lopezro.2021@alumnos.urjc.es>
2025-01-26 16:47:50 +0100

    Saco stlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: almlopez <almlopez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: almudenalopezrodriguez <almudenalopezro    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 12:25:09 by almlopez          #+#    #+#             */
-/*   Updated: 2025/01/24 13:04:40 by almlopez         ###   ########.fr       */
+/*   Updated: 2025/01/26 16:46:48 by almudenalop      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	m;
 	size_t	n;
-	size_t	s;
 	size_t	i;
 
 	m = ft_strlen(dst);
 	n = ft_strlen(src);
-	s = size - m;
 	i = 0;
 	if (size <= m)
-	{
 		return (n + size);
-	}
-	if (s > 0)
+	while (src[i] != '\0' && (m + i) < (size - 1))
 	{
-		while (src[i] != '\0' && i < size - 1)
-		{
-			dst[m + i] = src[i];
-			i++;
-		}
-		dst[m + i] = '\0';
-		
+		dst[m + i] = src[i];
+		i++;
 	}
+	dst[m + i] = '\0';
 	return (m + n);
 }
